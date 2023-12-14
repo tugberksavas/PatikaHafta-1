bf57267ae3c12ffb919e53c350a068493b94b1f1
tugberkgithub <111705123+tugberksavas@users.noreply.github.com>
2023-12-14 17:33:04 +0300

    GirilenSayilarin4Ve5inKuvvetleri

---
+++ GirilenSayilarinKucukKuvvetleriniBulma/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+
+
+        System.out.print("Sayı giriniz: ");
+        n=input.nextInt();
+        System.out.println("---------------------------");
+
+        System.out.println("Girilen sayının 2'ün kuvvetleri");
+
+        for(int i=1; i<=n;i*=2)
+        {
+            System.out.println(i);
+        }
+
+
+
+        System.out.println("Girilen sayının 4'ün kuvvetleri");
+        for (int i=1;i<=n;i*=4)
+            System.out.println(i);
+
+
+        System.out.println("Girilen sayının 5'in kuvvetleri");
+        for (int i=1;i<=n;i*=5)
+            System.out.println(i);
+
+
+    }
+}

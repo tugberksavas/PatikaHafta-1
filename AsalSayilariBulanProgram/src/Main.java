7509668dcc71578fa0c68aedd3b9afa776a71f8e
tugberkgithub <111705123+tugberksavas@users.noreply.github.com>
2023-12-17 14:09:30 +0300

    AsalSayilariBulanProgram

---
+++ AsalSayilariBulanProgram/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        //sayı istiyoruz
+
+        System.out.println("1 ile 100 arasında ki asal sayılar :");
+
+        //asal sayıları for loop ilee yazdırıyoruz
+        for(int number = 2; number < 100; number++){
+
+            //asal olup olmadığının kontrolünü boolean bir değişken ile sağlarız
+            boolean isPrime = true;
+
+            for(int i = 2; i < number; i++){
+                if(number % i == 0){
+
+                    //değişkenin kalansız bölündüğü bir sayı var ise boolean false olacağından yazdırmıyoruz
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if(isPrime){
+                System.out.print(number + " ");
+            }
+        }
+
+
+
+    }
+}

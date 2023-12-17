a28b5b6937dc3b07f6fa2b5cd76799c737fe5fa6
tugberkgithub <111705123+tugberksavas@users.noreply.github.com>
2023-12-17 14:07:00 +0300

    Mukemmel Sayi Bulma

---
+++ MukemmelSayiBulanProgram/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        //sayı istenşr
+        System.out.print("Bir sayı giriniz: ");
+        int number = scanner.nextInt();
+
+        int total  = 0;
+
+
+        //girilen sayıya kadar olan sayıların kalansız bölenleri total değerine eklenir
+        for(int i = 1; i < number; i++ ){
+            if(number != 1 && number % i == 0){
+                total += i;
+            }
+        }
+        //eğer total değişkeninin son değeri girilen sayıya eşitse mükemmel sayıdır.
+
+        if(total == number){
+            System.out.println(number + " mükemmel sayıdır");
+        } else {
+            System.out.println(number + " mükemmel sayı değildir");
+        }
+    }
+}

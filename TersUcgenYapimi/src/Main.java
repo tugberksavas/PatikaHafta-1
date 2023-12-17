9507473052294ab2d353bf08e7ea184695c8e6fa
tugberkgithub <111705123+tugberksavas@users.noreply.github.com>
2023-12-17 14:08:09 +0300

    TersUcgenYapimi

---
+++ TersUcgenYapimi/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        //digit sayısı istenir.
+
+        System.out.print("Basamak sayısını giriniz: ");
+        int digit = scanner.nextInt();
+
+        //girilen digit sayısına kadar yıldız koyulur ve sayı 2 2 azalarak o basamaklara boşluk koyulur
+        for (int i = digit; i >= 1; i--) {
+            for (int j = (digit - i); j >= 1; j--) {
+                System.out.print(" ");
+            }
+            for (int k = ((2 * i) - 1); k >= 1; k--) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
